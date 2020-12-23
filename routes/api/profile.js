cd561c7218129173a64a2fd56304a7bb457cb0b3
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2020-12-23 16:53:32 +0300

    routes - setup basic routes
    a basic setup just for testing

---
+++ routes/api/profile.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+// define the home page route
+router.get("/", (req, res) => {
+  res.send("profile home");
+});
+// define the about route
+router.get("/about", (req, res) => {
+  res.send("about profile");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const dbConnection = require("./config/db");
 
+const auth = require("./routes/api/auth");
+const profile = require("./routes/api/profile");
+const furniture = require("./routes/api/furniture");
+
+// create an app instance
 const app = express();
 
 // make db connection
 dbConnection();
+
+// routes
+app.use("/auth", auth);
+app.use("/profile", profile);
+app.use("/furniture", furniture);
 
 const port = process.env.PORT || 5000;
 
