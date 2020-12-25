3266be9927bd1c8a8bf786115cac2723186e44cf
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2020-12-25 17:02:10 +0300

    models - create an auth model
    represents a user in the auth sense and not in the profile sense

---
+++ models/Auth.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Auth Schema
+const AuthSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Auth = mongoose.model("auth", AuthSchema);
