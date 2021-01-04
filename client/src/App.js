5cf5fddd38574ed4a6653d379d336906c194ddfa
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2021-01-04 09:51:54 +0300

    config - test to see whether tailwindcss is working
    add some tailwindcss classnames for testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 const App = () => (
-  <div className="App">
-    <header className="App-header">elma's furniture</header>
+  <div>
+    <header className="bg-red-400 container h-60 w-20">
+      <p className="text-lg text-white">elma's furniture</p>
+    </header>
+    <div className="bg-red-500 text-white h-50">loremfnjbfbvjbvkjbvkrbvk</div>
   </div>
 );
 
