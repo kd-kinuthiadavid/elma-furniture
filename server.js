edce58e596d29b3e4187b0b2e2768c08e7f64aad
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2021-01-04 08:08:53 +0300

    routes - register the '/orders' routes

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const dbConnection = require("./config/db");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
+// import routes
 const auth = require("./routes/api/auth");
 const profile = require("./routes/api/profile");
 const furniture = require("./routes/api/furniture");
+const orders = require("./routes/api/orders");
 
 // create an app instance
 const app = express();
@@ -23,10 +25,11 @@ app.use(bodyParser.json());
 // passport middleware
 app.use(passport.initialize());
 
-// routes
+// register routes
 app.use("/api/auth", auth);
 app.use("/api/profile", profile);
 app.use("/api/furniture", furniture);
+app.use("/api/orders", orders);
 
 const port = process.env.PORT || 5000;
 
